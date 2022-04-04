b1c51a4876096a697b4fc72a4716fa4da1a62bee
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-04-04 22:25:26 +0300

    tasks 09-10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const colorValueTextRef = document.querySelector(".color");
+const changeColorButtonRef = document.querySelector(".change-color");
+changeColorButtonRef.addEventListener("click", () => {
+	bodyRef.style.backgroundColor = getRandomHexColor();
+	colorValueTextRef.textContent = getRandomHexColor();
+});
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBoxes = (amount) => {
+	const arrayOfDivsRef = [];
+	let startWidth = 30;
+	let startHeight = 30;
+	for (let i = 0; i < amount; i += 1) {
+		const divRef = document.createElement("div");
+		divRef.style.width = `${startWidth}px`;
+		divRef.style.height = `${startHeight}px`;
+		divRef.style.backgroundColor = getRandomHexColor();
+		startHeight += 10;
+		startWidth += 10;
+		arrayOfDivsRef.push(divRef);
+	}
+	inputRef.value = "";
+	return arrayOfDivsRef;
+};
+
+const destroyBoxes = () => {
+	divBoxRef.innerHTML = "";
+};
+
+const inputRef = document.querySelector("input");
+const createButtonRef = document.querySelector("button[data-create]");
+const destroyButtonRef = document.querySelector("button[data-destroy]");
+const divBoxRef = document.querySelector("#boxes");
+
+createButtonRef.addEventListener("click", () => {
+	divBoxRef.append(...createBoxes(inputRef.value));
+});
+
+destroyButtonRef.addEventListener("click", destroyBoxes);
